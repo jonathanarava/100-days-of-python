a3a301e8ce099921e9c7c9565564777439176e03
Jonathan Arava <32462175+jonathanarava@users.noreply.github.com>
2020-12-19 11:26:03 -0500

    Day 1 files upload complete

---
+++ day-1/day-1-4-exercise/main.py
@@ -0,0 +1,22 @@
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+# idea: you have a cup of coffee and a cup of milk and you want to switch the contents
+temp = a
+a = b
+b = temp
+
+
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
+
+
